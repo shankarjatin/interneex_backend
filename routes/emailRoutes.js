7f47a6ee4fc56d7e5cabcb37f1e88d21184592fb
shankarjatin <shankarjatin1005@gmail.com>
2024-08-14 18:15:44 +0530

    done

--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 // Health Check Route
 router.get('/health', (req, res) => {
-  res.status(200).json({ message: 'Backend is running properly.' });
+  res.status(200).json({ message: 'Backend is running properly. aaj' });
 });
 
 router.post('/send', sendEmail);
