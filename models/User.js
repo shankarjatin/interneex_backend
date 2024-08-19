5b83f4ccee00d82eda961c380c5d9cf816fe5fac
shankarjatin <shankarjatin1005@gmail.com>
2024-08-19 13:18:28 +0530

    auth model , user

---
+++ models/User.js
@@ -0,0 +1,52 @@
+// models/User.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    number: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    courseBought: {
+        type: Boolean,
+        default: false,
+    },
+    courseDetail: {
+        type: mongoose.Schema.Types.Mixed,
+        default: null,
+    },
+}, {
+    timestamps: true,
+});
+
+// Encrypt the password before saving the user
+UserSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+// Match user-entered password to hashed password in the database
+UserSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
