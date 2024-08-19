2b2a9683f8a9e985642e27282d4ca4d5224ce178
shankarjatin <shankarjatin1005@gmail.com>
2024-08-19 18:56:11 +0530

    aaj ka change

--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 // Health Check Route
 router.get('/health', (req, res) => {
-  res.status(200).json({ message: 'Backend is running properly. aaj' });
+  res.status(200).json({ message: 'Backend is running properly. aaj 19' });
 });
 
 router.post('/send', sendEmail);
