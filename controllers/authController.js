0ff69a26c818efb73d5a44c4a36113448b5dc96b
shankarjatin <shankarjatin1005@gmail.com>
2024-08-21 14:14:46 +0530

    reset pasword

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,11 @@
 // controllers/authController.js
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const crypto = require('crypto');
+const nodemailer = require('nodemailer');
+const User = require('../models/User');
+const transporter = require('../config/nodemailerConfig')
+
 
 const generateToken = (user) => {
     return jwt.sign(
@@ -112,8 +117,93 @@ const loginUser = async (req, res) => {
     }
 };
 
+
+const forgotPassword = async (req, res) => {
+    const { email } = req.body;
+
+    try {
+        // Check if user exists
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        // Generate reset token
+        const resetToken = user.createPasswordResetToken();
+        await user.save({ validateBeforeSave: false });
+
+        // Create reset URL
+        const resetUrl = `${req.protocol}://${req.get('host')}/api/auth/reset-password/${resetToken}`;
+
+        // Send email
+        const message = `You are receiving this email because you (or someone else) have requested the reset of a password. Please make a put request to: \n\n ${resetUrl}`;
+
+   
+
+        await transporter.sendMail({
+            to: user.email,
+            subject: 'Password Reset Request',
+            text: message,
+        });
+
+        res.status(200).json({ success: true, message: 'Email sent' });
+    } catch (error) {
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpires = undefined;
+        await user.save({ validateBeforeSave: false });
+
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+// @desc    Reset password
+// @route   PUT /api/auth/reset-password/:token
+// @access  Public
+const resetPassword = async (req, res) => {
+    const { token } = req.params;
+    const { password } = req.body;
+
+    try {
+        // Hash the token and find the user
+        const hashedToken = crypto.createHash('sha256').update(token).digest('hex');
+        const user = await User.findOne({
+            resetPasswordToken: hashedToken,
+            resetPasswordExpires: { $gt: Date.now() },
+        });
+
+        if (!user) {
+            return res.status(400).json({ message: 'Token is invalid or has expired' });
+        }
+
+        // Set the new password
+        user.password = password;
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpires = undefined;
+        await user.save();
+
+        // Generate token
+        const newToken = generateToken(user);
+
+        res.status(200).json({
+            success: true,
+            token: newToken,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                number: user.number,
+            },
+        });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+
 module.exports = {
     registerUser,
     loginUser,
-    getUserInfo
+    getUserInfo,
+    forgotPassword,
+    resetPassword,
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,13 @@
 // routes/auth.js
 const express = require('express');
-const { registerUser, loginUser, getUserInfo} = require('../controllers/authController');
+const { registerUser, loginUser, getUserInfo , forgotPassword , resetPassword} = require('../controllers/authController');
 const authMiddleware = require('../middleware/auth');
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/user', authMiddleware, getUserInfo);
+router.get('/forgot-password', forgotPassword);
+router.put('/reset-password/:token', resetPassword);
 
 module.exports = router;
