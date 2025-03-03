5c76e8f02b406da690786788cdfaaf070e9151b6
shankarjatin <shankarjatin1005@gmail.com>
2025-03-03 18:44:08 +0530

    doe

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const cron = require('cron');
 
 require('dotenv').config();
 
@@ -40,6 +41,22 @@ app.use('/api', paymentRoutes);
 app.use("/api", userRoutes);
 app.use('/api', privateRoute);
 app.use('/api', adminRoute);
+app.get('/', (req, res) => {
+  res.send('Server is running and active!');
+  console.log('Server is running and active!');
+});
+
+// Schedule a cron job to run every 5 minutes to keep the server alive
+cron.schedule('**/2 * * * * *', async () => {
+  try {
+    console.log('Pinging server to keep it awake...');
+    // Change this to your actual server's public URL
+    await axios.get('https://tutedude-x2vx.onrender.com');
+    console.log('Server pinged successfully');
+  } catch (error) {
+    console.error('Error pinging the server:', error.message);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
